cc04593fd651d551ae716357508b2999d0022d84
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2025-09-15 00:22:26 +0100

    Add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,62 @@
+/*!
+Package Family Name is a Rust crate for calculating MSIX Package Family Name values.
+
+Every MSIX application has a package family name value, which looks a bit like
+`AppName_zj75k085cmj1a`. This value can easily be found by running `Get-AppxPackage <name>` in
+PowerShell for an installed MSIX package and scrolling to `PackageFullName`.
+
+However, we can work out a package family name value without needing to install the package at all.
+That's where this library comes into play.
+
+## Usage
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+package-family-name = "2"
+```
+
+```
+# use package_family_name::PackageFamilyName;
+let package_family_name = PackageFamilyName::new(
+    "Microsoft.PowerShell",
+    "CN=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=Washington, C=US"
+);
+
+assert_eq!(package_family_name.to_string(), "Microsoft.PowerShell_8wekyb3d8bbwe");
+```
+
+## How a package family name is calculated
+
+In short, a package family name is made up of two parts:
+
+- Identity name (`Microsoft.PowerShell`)
+- Identity publisher (`CN=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=Washington, C=US`)
+
+These steps are then taken:
+
+1. UTF-16 encode the identity publisher
+2. Calculate a SHA256 hash of the encoded publisher
+3. Take the first 8 bytes of the hash
+4. Encode the result with [Douglas Crockford Base32](http://www.crockford.com/base32.html)
+5. Join the identity name and the encoded value with an underscore (`Microsoft.PowerShell_8wekyb3d8bbwe`)
+
+### Why would I need to calculate a package family name?
+
+Whilst this is a niche library, there are use cases. For example, when submitting an MSIX package to
+[winget-pkgs](https://github.com/microsoft/winget-pkgs), a package family name value is a required
+as part of the manifest.
+
+## Acknowledgements
+
+[@marcinotorowski](https://github.com/marcinotorowski) has produced a step by step explanation of
+how to calculate the hash part of the package family name.
+This post can be found
+[here](https://marcinotorowski.com/2021/12/19/calculating-hash-part-of-msix-package-family-name).
+*/
+
+#![doc(html_root_url = "https://docs.rs/package-family-name")]
 #![forbid(unsafe_code)]
 #![no_std]
 
